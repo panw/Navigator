d09c415ba51bff583b4bf3bae80dd0e0b1dd481b
Pan Wangperawong <panthongw@gmail.com>
2018-12-20 21:41:44 -0500

    Add button navigation between screens

--- src/components/DetailsScreen.js
+++ src/components/DetailsScreen.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 
 export default class DetailsScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
       </View>
     );
   }
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
       </View>
     );
   }
