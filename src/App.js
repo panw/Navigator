98571ff9adc68066794b43838d3e1af9a024a691
Pan Wangperawong <panthongw@gmail.com>
2018-12-20 21:31:32 -0500

    Setup app navigator with two screens

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import HomeScreen from './components/HomeScreen';
+import React, {Component} from 'react'
+import {Platform, StyleSheet, Text, View} from 'react-native'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import HomeScreen from './components/HomeScreen'
+import DetailsScreen from './components/DetailsScreen'
 
 export default class App extends Component<Props> {
   render() {
     return (
-      <HomeScreen />
+      <AppContainer />
     );
   }
 }
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
